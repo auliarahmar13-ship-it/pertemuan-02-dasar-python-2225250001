67caebf7708c3898627eadc8604fc418a0e0d916
auliarahmar13-ship-it <auliarahmar13@gmail.com>
2026-09-08 22:52:44 -0700

    Update 03_konversi_suhu.py

--- Pertemuan-02/latihan/03_konversi_suhu.py
+++ Pertemuan-02/latihan/03_konversi_suhu.py
@@ -1,1 +1,14 @@
 
+# Latihan 3 Konversi Suhu
+
+KELVIN_OFFSET = 273.15
+
+celsius = float(input("Masukkan suhu Celsius: "))
+
+fahrenheit = (9/5) * celsius + 32
+kelvin = celsius + KELVIN_OFFSET
+
+print("\n===== HASIL KONVERSI =====")
+print(f"Celsius    : {celsius:.2f} °C")
+print(f"Fahrenheit : {fahrenheit:.2f} °F")
+print(f"Kelvin     : {kelvin:.2f} K")
