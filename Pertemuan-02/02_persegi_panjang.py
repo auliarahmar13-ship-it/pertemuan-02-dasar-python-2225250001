ef2108451a5190dc703618e982f802990da21f8d
auliarahmar13-ship-it <auliarahmar13@gmail.com>
2026-09-08 22:02:00 -0700

    Add files via upload

---
+++ Pertemuan-02/02_persegi_panjang.py
@@ -0,0 +1,14 @@
+
+# Latihan 2 Persegi Panjang
+
+panjang = float(input("Masukkan panjang (cm): "))
+lebar = float(input("Masukkan lebar (cm): "))
+
+luas = panjang * lebar
+keliling = 2 * (panjang + lebar)
+
+print("\n===== HASIL PERHITUNGAN =====")
+print(f"Panjang  : {panjang:.2f} cm")
+print(f"Lebar    : {lebar:.2f} cm")
+print(f"Luas     : {luas:.2f} cm²")
+print(f"Keliling : {keliling:.2f} cm")
